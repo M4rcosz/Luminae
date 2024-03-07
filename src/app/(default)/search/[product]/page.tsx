0801ab71cde349445fafabb3d39e53fcfe75802a
M4rcosz <ijohnfelipe71@gmail.com>
2024-03-07 17:54:20 -0300

    FIX: Tentativa de correção de Deploy 1

--- src/app/(default)/search/[product]/page.tsx
+++ src/app/(default)/search/[product]/page.tsx
@@ -8,11 +8,11 @@ const SearchProducts = async ({ params }: { params: { product: string } }) => {
         <main>
             {hasFounded === true ?
                 <div className="container mx-auto my-3">
-                    <p className="px-Mobile md:px-Tablet md:my-5 lg:my-0 italic font-bold">Searching by  "<span className="text-blue-700">{params.product}</span>":</p>
+                    <p className="px-Mobile md:px-Tablet md:my-5 lg:my-0 italic font-bold">Searching by  "{<span className="text-blue-700">{params.product}</span>}":</p>
                 </div>
                 :
                 <div className="container mx-auto px-Mobile md:px-Tablet my-3">
-                    <p className="md:my-5 lg:my-0 italic font-bold">Not found by "<span className="text-red-700">{params.product}</span>"</p>
+                    <p className="md:my-5 lg:my-0 italic font-bold">Not found by "{<span className="text-red-700">{params.product}</span>}"</p>
                 </div>
             }
 
